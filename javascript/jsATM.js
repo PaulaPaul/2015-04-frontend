7efaa5d1e439b3462246e2e662eee3e7a2a65c34
PaulaTTS <paula@techtalentsouth.com>
2015-05-28 17:40:50 +0000

    cleanup and prep for Full Time lesson on JavaScript/jQuery/Angular ATMs

--- javascript/jsATM.js
+++ javascript/jsATM.js
@@ -1,26 +1,35 @@
-  var c_balance = 5;
-  var s_balance = 0;
+  // JavaScript ATM implementation
+  var c_balance = 0.0;
+  var s_balance = 0.0;
 
   function deposit() {
-    var trxamt = document.getElementById("d_amount").value;
+    var trxamt = parseFloat(document.getElementById("d_amount").value);
+    //write that code!
     alert(trxamt);
+    document.getElementById("message").innerText = 'Your deposit of $' + trxamt + ' to checking is complete.';
   }
 
   function withdraw() {
-    var trxamt = document.getElementById("w_amount").value;
+    var trxamt = parseFloat(document.getElementById("w_amount").value);
+    //write that code!
     alert(trxamt);
+    document.getElementById("message").innerText = 'Your withdrawal of $' + trxamt + ' from checking is complete.';
   }
 
   function x2c() {
-    var trxamt = document.getElementById("x2c_amount").value;
+    var trxamt = parseFloat(document.getElementById("x2c_amount").value);
+    //write that code!
     alert(trxamt);
+    document.getElementById("message").innerText = 'Your transfer of $' + trxamt + ' from savings to checking is complete.';
   }
 
   function x2s() {
-    var trxamt = document.getElementById("x2c_amount").value;
+    var trxamt = parseFloat(document.getElementById("x2s_amount").value);
+    //write that code!
     alert(trxamt);
+    document.getElementById("message").innerText = 'Your transfer of $' + trxamt + ' from checking to savings is complete.';
   }
 
   function getbalance() {
-    alert(c_balance);
+    document.getElementById("message").innerText = 'Your checking account balance is: $' + c_balance + ', and your savings balance is: $' + s_balance;
   }
